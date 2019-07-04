ff88f142f600bfb3ec0afd2ee312d40fee584a3e
Robert Battaglia <r083rt164@gmail.com>
2019-07-04 13:53:59 -0400

    add a few different components and links to them with react router

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Home from './Home.jsx';
+import AboutUs from './AboutUs.jsx';
 
 class App extends Component {
   render() {
     return (
-      <React.Fragment>
-        <h1>Hello World</h1>
-      </React.Fragment>
+      <Router>
+        <React.Fragment>
+          <nav>
+            <Link to="/home">Home</Link>
+            <Link to="/aboutus">About Us</Link>
+          </nav>
+          <Route path="/home" component={Home} />
+          <Route path="/aboutus" component={AboutUs} />
+        </React.Fragment>
+      </Router>
     );
   }
 }
