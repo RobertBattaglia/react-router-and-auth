b6d4059cbeeac6d9b2330d21f06dd5e47652793b
Robert Battaglia <r083rt164@gmail.com>
2019-07-04 21:55:49 -0400

    server responding to all requests with index.html except for bundle.js

--- server/index.js
+++ server/index.js
@@ -5,6 +5,26 @@ const app = express();
 
 app.use('/', express.static(path.join(__dirname, '..', 'client', 'dist')));
 
+app.get('/bundle.js', (req, res) => {
+  res.sendFile(__dirname, '..', 'client', 'dist', 'bundle.js'),
+    err => {
+      if (err) {
+        res.status(500).send(err);
+      }
+    };
+});
+
+app.get('/*', function(req, res) {
+  res.sendFile(
+    path.join(__dirname, '..', 'client', 'dist', 'index.html'),
+    err => {
+      if (err) {
+        res.status(500).send(err);
+      }
+    }
+  );
+});
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, err => {
