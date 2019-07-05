3c2049b977950fdcbb3b338dcef432114158821a
Robert Battaglia <r083rt164@gmail.com>
2019-07-04 22:36:34 -0400

    add 404 when react router doesn't have path

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import Home from './Home.jsx';
 import AboutUs from './AboutUs.jsx';
+import NoMatch from './NoMatch.jsx';
 
 class App extends Component {
   render() {
@@ -12,8 +13,11 @@ class App extends Component {
             <Link to="/home">Home</Link>
             <Link to="/aboutus">About Us</Link>
           </nav>
-          <Route path="/home" component={Home} />
-          <Route path="/aboutus" component={AboutUs} />
+          <Switch>
+            <Route path="/home" component={Home} />
+            <Route path="/aboutus" component={AboutUs} />
+            <Route component={NoMatch} />
+          </Switch>
         </React.Fragment>
       </Router>
     );
---
+++ client/src/components/NoMatch.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NoMatch = () => {
+  return (
+    <React.Fragment>
+      <h1>404</h1>
+    </React.Fragment>
+  );
+};
+
+export default NoMatch;
